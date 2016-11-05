a6620b70911458c63ca610065264c27015e41a6f
Shahzil Aftab <Shahzil Aftab>
2016-11-05 14:03:49 -0500

    Some backend reconfig

---
+++ old.js
@@ -0,0 +1,32 @@
+//**** Original File - Deprecated *****
+//sffc
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override'); // lets us use PUT and DELETE even if client doesn't support it
+
+
+
+
+app.use(express.static(__dirname + '/dist')); // set the static files location /public/img will be /img for users
+
+
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+
+app.get('*', function(req, res) {
+
+    res.sendfile('./dist/index.html'); // loads the single view file (angular)
+
+});
+
+app.listen(process.env.PORT || 3000);
+console.log("App listening on port 3000");
