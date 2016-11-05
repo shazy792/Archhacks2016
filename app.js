82c678a3d72a5a7a67891bedf7611e78ca0bf775
Shahzil Aftab <Shahzil Aftab>
2016-11-05 04:43:51 -0500

    API all set

--- app.js
+++ app.js
@@ -13,19 +13,38 @@ app.use(express.static(__dirname + '/dist')); // set the static files location /
 //app.use(morgan('dev')); //Not using Morgan
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+//app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(methodOverride());
 
-mongoose.connect("mongodb://localhost/testt");
+mongoose.connect("mongodb://localhost/bracelets");
 
 // This is the mongo model - Change this!
-var Resource = app.resource = restful.model('yo', mongoose.Schema({
-    x: Number,
+var Restapi = app.restapi = restful.model('bracelet', mongoose.Schema({
+    rfid: String,
+    weight: Number,
+    height: String,
+    name: String,
+    age: Number,
+    btype: String,
+    allergies: [],
+    pproblems: [],
+    emergency: String
   }))
   .methods(['get', 'post', 'put', 'delete']);
 
+  Restapi.before('get', isAdmin);
+
+  function isAdmin(req, res, next){
+  	if (req) { // check authentication over here
+  		next();
+  	} else {
+  		res.send('Not logged in');
+  	}
+  }
+
+
 // Registers the REST on /resources 
-Resource.register(app, '/d');
+Restapi.register(app, '/api');
 
 app.listen(3000, function(){
 	console.log("Server Started");
